00189a757809fc4f5cd9eb064139e3fa771d0207
Joe Siburov <g.siburov@gmail.com>
2026-01-27 17:55:28 -0800

    build(app): DB connection + packages upd

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -21,13 +23,12 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
+  try{
+      await connectDB(process.env.MONGO_URI)
+      app.listen(port, console.log(`Server is running on ${port}`))
+  } catch (err) {
+      console.log(err)
   }
-};
+}
 
 start();
